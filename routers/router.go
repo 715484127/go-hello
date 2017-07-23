9efca0b5d78ba70a564b515eea2dcc6b655481d2
wangyuepeng <715484127@qq.com>
2017-07-23 10:24:54 +0800

    init

---
+++ routers/router.go
@@ -0,0 +1,12 @@
+package routers
+
+import (
+	"hello/controllers"
+
+	"github.com/astaxie/beego"
+)
+
+func init() {
+	beego.Router("/", &controllers.MainController{})
+	beego.Router("/hello-world/:id([0-9]+)", &controllers.MainController{}, "get:HelloSitepoint")
+}
